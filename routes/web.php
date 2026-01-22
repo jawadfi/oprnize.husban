fbc4cd53b2f807f472ffffd10fc275a6bd90dbf7
MahmoudSalameh <mahmoudslameh95@gmail.com>
2026-01-22 23:58:30 +0200

    fix

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/roleCompany', function () {
+    /** @var \App\Models\Company $company */
+    foreach (\App\Models\Company::all() as $company)
+        $company->assignRole('super_admin');
+});
 Route::get('mail', function () {
     Mail::raw('Hello John, this is a simple plain-text message.', function ($message) {
         $message->to('mahmoudslameh95@gmail.com')
